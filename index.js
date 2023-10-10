72d6ba10fd15ec7c6a9662ae6a4b9cd72dfdbb75
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-10-10 13:58:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello worrld!");
+console.log("it's really good!");
+
+window.alert("I really love pizza!");
+
+//This is a comment!
+
+/*This
+ is
+  a 
+  multiline 
+  comment
+  
+*/
